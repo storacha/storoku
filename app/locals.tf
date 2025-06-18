1fcfcc4c08cb50ce7abf7fff577d0a97892eb855
Vicente Olmedo <vicente@storacha.network>
2025-06-18 16:23:20 +0200

    don't use invented functions

--- app/locals.tf
+++ app/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  is_production = var.environment == "prod" || starts_with("prod-", var.environment) || ends_with("-prod", var.environment)
-  is_staging = var.environment == "staging" || starts_with("staging-", var.environment) || ends_with("-staging", var.environment)
+  is_production = var.environment == "prod" || startswith(var.environment, "prod-") || endswith(var.environment, "-prod")
+  is_staging = var.environment == "staging" || startswith(var.environment, "staging-") || endswith(var.environment, "-staging")
 
   # Only prod and staging get their own resources. All other envs will share the dev shared infra
   dedicated_resources = local.is_production || local.is_staging
