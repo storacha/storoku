586c5d64ca04d66decce5480a026aa157d89c9ef
Vicente Olmedo <vicente@storacha.network>
2025-07-04 19:13:31 +0200

    feat: add the task execution role to the deployment module outputs (#16)

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -2,6 +2,10 @@ output "task_definition" {
   value = aws_ecs_task_definition.app
 }
 
+output "task_role" {
+  value = aws_iam_role.ecs_task_role
+}
+
 output "service" {
   value = aws_ecs_service.service
 }
